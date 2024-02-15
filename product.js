daed24e49d53f8e7424cfc3da4dce649db9fe2c5
JOSE <jose@iMac-de-JOSE.local>
2024-02-15 20:42:30 +0100

    trabajo terminado

--- product.js
+++ product.js
@@ -1,17 +1,25 @@
 let products = [];
 let id = 0;
 
-const addProduct = (nombre, precio) => {
+const addProduct = (name, price) => {
   const newID = products.length;
-  const existingProduct = products.find((product) => product.nombre === nombre);
+  const existingProduct = products.find((product) => product.name === name);
 
   if (existingProduct) {
-    throw new Error('El producto ya existe');
+    throw new Error('product must be different');
+  }
+
+  if (!name) {
+    throw new Error('product must have a name');
+  }
+
+  if (!price) {
+    throw new Error('product must have a price');
   }
 
   products[newID] = {
-    nombre,
-    precio
+    name: name,
+    price: price
   }
 
   return products;
@@ -23,20 +31,31 @@ const resetProducts = () => {
   return products;
 }
 
-const removeProduct = () => {
+const removeProduct = (id) => {
+  products.splice(id, 1);
 
+  return products;
 }
 
 const getProducts = () => {
   return products;
 }
 
-const getProduct = () => {
-
+const getProduct = (id) => {
+  return products[id];
 }
 
-const updateProduct = () => {
+const updateProduct = (id, name, price) => {
+  if (!products[id]) {
+    throw new Error('product must exist');
+  }
 
+  products[id] = {
+    name: name,
+    price: price
+  };
+
+  return products;
 }
 
 module.exports = { resetProducts, addProduct, removeProduct, getProducts, getProduct, updateProduct };
--- product.test.js
+++ product.test.js
@@ -6,11 +6,13 @@ beforeEach(() => {
 
 describe('Adding Products', () => {
   it('should add a product', () => {
-    expect(addProduct('Producto 1', 5)).toEqual([{ nombre: 'Producto 1', precio: 5 }]);
+    expect(addProduct('Producto 1', 5)).toEqual([{ name: 'Producto 1', price: 5 }]);
   });
 
   it('should fail when adding a repeated product', () => {
-    expect(() => addProduct('Producto 1', 5)).toThrowError('product must be different');
+    addProduct('Producto 1', 5);
+
+    expect(() => addProduct('Producto 1', 5)).toThrow('product must be different');
   });
 
   it('should fail when adding a product with no name', () => {
@@ -24,30 +26,31 @@ describe('Adding Products', () => {
 
 describe('Removing Products', () => {
   it('should remove a product', () => {
-    expect()
+    addProduct('Producto 1', 2);
+    addProduct('Producto 2', 5);
+
+    expect(removeProduct(1)).toEqual([{ name: 'Producto 1', price: 2 }]);
   });
 });
 
 describe('Getting a single product', () => {
   it('should get a product', () => {
-    expect()
+    addProduct('Producto 1', 2);
+    addProduct('Producto 2', 5);
+
+    expect(getProduct(0)).toEqual({ name: 'Producto 1', price: 2 });
   });
 });
 
 describe('Updating Products', () => {
   it('should update a product', () => {
-    expect()
+    addProduct('Producto 1', 2);
+    addProduct('Producto 2', 5);
+
+    expect(updateProduct(0, 'Producto Uno', 3)).toEqual([{ name: 'Producto Uno', price: 3 }, { name: 'Producto 2', price: 5 }]);
   });
 
   it('should fail when updating a product that does not exist', () => {
-    expect()
-  });
-
-  it('should only update the price', () => {
-    expect()
-  });
-
-  it('should only update the name', () => {
-    expect()
+    expect(() => updateProduct(1, 'Producto X', 63)).toThrow('product must exist');
   });
 });
